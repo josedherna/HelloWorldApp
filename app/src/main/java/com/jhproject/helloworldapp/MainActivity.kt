3471ab473f749470584c777f0fcad3380316ba20
josedherna <157747148+josedherna@users.noreply.github.com>
2025-09-01 23:52:21 -0400

    feat: added a button that displays Hello World

--- app/src/main/java/com/jhproject/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/jhproject/helloworldapp/MainActivity.kt
@@ -4,44 +4,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jhproject.helloworldapp.ui.theme.HelloWorldAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HelloWorldAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ParentComposable()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+var message = ""
+
+fun greeting() {
+    message = "Hello World!"
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HelloWorldAppTheme {
-        Greeting("Android")
+fun ParentComposable() {
+    var greetingText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = greetingText, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            greetingText = "Hello World"
+        }
+        ) {
+            Text("Show Greeting");
+        }
     }
 }
